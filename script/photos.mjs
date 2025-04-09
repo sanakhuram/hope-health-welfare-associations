eb032f757666f57cec221dc64211d85e44c2ac59
Sana Khuram <sana.khuram.baig@gmail.com>
2025-04-09 15:06:36 +0200

    updated images and captions

--- script/photos.mjs
+++ script/photos.mjs
@@ -1,11 +1,11 @@
 export function initPhotoGallery() {
   const photoGallery = document.getElementById('photo-gallery');
 
-  // Check if photoGallery exists before proceeding
   if (!photoGallery) {
     console.log('Photo gallery element not found');
-    return; // Exit the function early
+    return; 
   }
+
   const photos = [
     {
       src: './assets/images/1.jpg',
@@ -13,9 +13,9 @@ export function initPhotoGallery() {
     },
     { src: './assets/images/2.jpg', caption: 'Student Marriage 2018' },
     { src: './assets/images/3.jpg', caption: 'Opening ceremony of Stitch box' },
-    { src: './assets/images/4.jpg', caption: 'Caption for image 4' },
+    { src: './assets/images/4.jpg', caption: 'Yearly Marriage Ceremony' },
     { src: './assets/images/5.jpg', caption: 'Caption for image 5' },
-    { src: './assets/images/6.jpg', caption: 'Caption for image 6' },
+    { src: './assets/images/6.jpg', caption: 'Yearly Marriage Ceremony' },
     {
       src: './assets/images/7.jpg',
       caption: 'Mrs. Saima Ahad DCO Okara visit our Institute',
@@ -26,7 +26,7 @@ export function initPhotoGallery() {
     },
     { src: './assets/images/9.jpg', caption: 'Jashan e Eid milad un Nabi' },
     { src: './assets/images/10.jpg', caption: 'First Annual convocation 2016' },
-    { src: './assets/images/11.jpg', caption: 'Caption for image 11' },
+    { src: './assets/images/11.jpg', caption: 'Prize Distribution 2016' },
     { src: './assets/images/12.jpg', caption: 'Mrs. Zara muneeb visit' },
     {
       src: './assets/images/13.jpg',
@@ -46,14 +46,25 @@ export function initPhotoGallery() {
       caption: 'Ranal vocational institute student visit',
     },
     { src: './assets/images/18.jpg', caption: 'Caption for image 18' },
-
     {
       src: './assets/images/19.jpg',
-      caption: 'Visiting Faculty',
+      caption: 'Visiting Faculty Lecture on Self Grooming',
     },
-    
-    // Add more photos here
+    {
+      src: './assets/images/20.jpg',
+      caption: 'Visiting Faculty Lecture on Health Topics',
+    },
+    {
+      src: './assets/images/21.jpg',
+      caption: 'Award Distribution 2025',
+    },
+    {
+      src: './assets/images/22.jpg',
+      caption: 'yearly Marriage ceremony 2025',
+    },
   ];
+
+  photos.reverse();
 
   photos.forEach((photo) => {
     const photoDiv = document.createElement('div');
